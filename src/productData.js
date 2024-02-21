11643cbd8f5506bb56cae3a99d97f54c71754d9a
Alejandro Marín Astorga <ale.marincbz@outlook.es>
2024-02-20 23:48:45 -0300

    Enriquecimiento de productos X2

--- src/productData.js
+++ src/productData.js
@@ -38,36 +38,48 @@ const products = [
     title: "Zoom Vomero Nike Hombre Negro/Verde Musgo",
     price: "$187.000",
     image: ImgPrd4,
+    description:
+      "Explora tus límites con las Zoom Vomero de Nike, diseñadas para el hombre activo y dinámico. Su combinación de colores negro y verde musgo aporta un toque de elegancia discreta, mientras que la tecnología de amortiguación Zoom Air te garantiza una respuesta inigualable en cada paso.",
   },
   {
     id: 5,
     title: "Air Pegasus Nike Hombre Blanco/Negro",
     price: "$120.000",
     image: ImgPrd5,
+    description:
+      "Eleva tu rutina de running con las Air Pegasus de Nike, un verdadero clásico reimaginado para el corredor moderno. Estas zapatillas en blanco y negro no solo son llamativas, sino que también ofrecen un rendimiento excepcional gracias a su amortiguación premium y tracción superior.",
   },
   {
     id: 6,
     title: "Air Force 1 Nike Hombre Borgoña",
     price: "$112.990",
     image: ImgPrd6,
+    description:
+      "La icónica silueta Air Force 1 se tiñe con un elegante tono borgoña, ofreciendo una versión sofisticada de un clásico del streetwear. Perfectas para el hombre que busca destacar con estilo, estas zapatillas combinan la herencia de Nike con una paleta de colores moderna.",
   },
   {
     id: 7,
     title: "Dunk Low Nike Hombre Borgoña",
     price: "$87.990",
     image: ImgPrd7,
+    description:
+      "Revive la nostalgia de los 80 con las Dunk Low de Nike, aquí presentadas en un distintivo color borgoña. Estas zapatillas bajas no solo rinden homenaje a sus raíces de baloncesto, sino que también se adaptan a las tendencias actuales, asegurando que estés a la moda tanto dentro como fuera de la cancha.",
   },
   {
     id: 8,
     title: "Air 2 Retro Low Nike Negro/Verde/Rojo",
     price: "$259.999",
     image: ImgPrd8,
+    description:
+      "Captura la esencia del legado deportivo con las Air 2 Retro Low de Nike. Estas zapatillas, que hacen una audaz declaración con su combinación de negro intenso, verde vibrante y detalles en rojo, son una reinvención moderna de un diseño retro. Su bajo perfil proporciona versatilidad y estilo, mientras que la construcción de calidad asegura durabilidad y comodidad para el uso diario. Perfectas para aquellos que quieren rendir tributo a la historia del deporte con un toque contemporáneo.",
   },
   {
     id: 9,
     title: "New Balance 990v6 Hombre Negro/Gris",
     price: "$56.990",
     image: ImgPrd9,
+    description:
+      "Experimenta la evolución de la comodidad con las New Balance 990v6. Este modelo presenta una paleta de colores en negro y gris, ofreciendo una estética versátil para el hombre moderno. Su diseño se centra en la estabilidad y la soporte, con una entresuela que proporciona amortiguación excepcional para el día a día. Ya sea para entrenamientos serios o para recados casuales, estas zapatillas se convertirán en tus compañeras más confiables en cada paso que des.",
   },
 ];
 
